95146090afa1cc40aef25a8e8b621d89d90ea87d
chromadison <contact@madisonbeck.com>
2020-12-30 19:42:20 -0500

    Added amazon store link and updated products

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 // Fullpage
 $(document).ready(function () {
-    $("#fullpage").fullpage({
-      menu: "#menu",
-      anchors: ["intro", "about", "products", "contact"],
-      autoScrolling: true,
-      scrollHorizontally: true,
-      responsiveWidth: 1000,
-      afterLoad: function (origin) {
-        $(".intro").addClass("animate__fadeIn");
-      },
-      onLeave: function (origin, index) {
-        if ($(window).width() > 1000) {
-          if (origin.index === 0) {
-            $(".about").addClass("animate__fadeIn");
-          }
-          if (origin.index === 1) {
-            $(".products").addClass("animate__fadeIn");
-          }
-          if (origin.index === 2) {
-            $(".contact").addClass("animate__fadeIn");
-          }
+  $('#fullpage').fullpage({
+    menu: '#menu',
+    anchors: ['intro', 'about', 'products', 'contact'],
+    autoScrolling: true,
+    scrollHorizontally: true,
+    responsiveWidth: 1200,
+    afterLoad: function (origin) {
+      $('.intro').addClass('animate__fadeIn');
+    },
+    onLeave: function (origin, index) {
+      if ($(window).width() > 1200) {
+        if (origin.index === 0) {
+          $('.about').addClass('animate__fadeIn');
         }
-      },
-    });
-    // Intro section down arrow
-    $(".arrowDown").click(function () {
-      $.fn.fullpage.moveSectionDown();
-    });
+        if (origin.index === 1) {
+          $('.products').addClass('animate__fadeIn');
+        }
+        if (origin.index === 2) {
+          $('.contact').addClass('animate__fadeIn');
+        }
+      }
+    },
   });
+  // Intro section down arrow
+  $('.arrowDown').click(function () {
+    $.fn.fullpage.moveSectionDown();
+  });
+});
